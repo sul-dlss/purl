a2d211be9a8919cf58623adba0dcd44443e5cb20
Douglas Kim <dougkim@stanford.edu>
2010-06-16 16:35:22 -0700

    Display label on landing page for supplemental files.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,7 @@
 
 class Resource
 
-  attr_accessor :objectId, :type, :mimetype, :size, :sequence, :label, :filename, :url
+  attr_accessor :objectId, :type, :mimetype, :size, :sequence, :description_label, :filename, :url
 
   attr_accessor :shelve, :preserve, :deliver
   
