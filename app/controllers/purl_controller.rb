7361515aaf29128fa4d625fd7fa8e7a19a20028b
Douglas Kim <dougkim@stanford.edu>
2010-06-16 12:09:01 -0700

    Check the shelve workflow status to determine whether to deliver the landing page.

--- app/controllers/purl_controller.rb
+++ app/controllers/purl_controller.rb
@@ -8,36 +8,30 @@ class PurlController < ApplicationController
   def index
     @purl = Purl.new
     @purl.retrieve_metadata(params[:id])
-    if(@purl.xml == '')
-      render :file => "purl/unavailable.html.erb", :layout => "layouts/unavailable.html.erb"
-    else
-      respond_to do |format|
-        format.xml { render :xml => @purl.xml }
-        format.html
-      end
+
+    # validate that the id is ready for delivery
+    if( @purl.content == '' && !Dor::Util.is_shelved?(params[:id]) )
+      render :partial => "purl/unavailable", :layout => "application"
+      return false
+    end
+
+    # render the landing page based on the format
+    respond_to do |format|
+      format.xml { render :xml => @purl.xml }
+      format.html
     end
   end
 
   private
 
+  # validate that the id is of the proper format
   def validate_id
-    # validate that the id is of the proper format
     if( !Dor::Util.validate_druid(params[:id]) )
-      # TODO : dougkim
-      # render some formatted page instead of a 404 page
       render :status => 404, :file => "#{RAILS_ROOT}/public/404.html"
       return false
     end
-    
-    # validate that the id exists in the digital object repository
-    if( !Dor::Util.exists_resource(params[:id]) )
-      render :status => 404, :file => "#{RAILS_ROOT}/public/404.html"
-      return false
-    end
-
-    # at the point assume the id is valid and the resource exists
     true
   end
-  
+
 end
 
