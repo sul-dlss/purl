1a7492ba3c34da8f0b680b0b652d2c4fc59b69c8
Douglas Kim <dougkim@stanford.edu>
2010-07-09 15:42:17 -0700

    Retrieve metadata from document cache as opposed to querying DOR.

--- app/helpers/purl_helper.rb
+++ app/helpers/purl_helper.rb
@@ -1,9 +1,29 @@
 
 module PurlHelper
 
+  #
+  # This method round the given number to the given number of decimal points
+  #
   def round_to(num,decimals=0)
     factor = 10.0**decimals
     (num*factor).round / factor
   end
-  
+
+  #
+  # This method returns the pair tree directory structure based on the given object identifier.
+  # The object identifier must be of the following format, otherwise nil is returned.
+  #
+  #     druid:xxyyyxxyyyy
+  #
+  #       where 'x' is an alphabetic character
+  #       where 'y' is a numeric character
+  #
+  def create_pair_tree(pid)
+    if(pid =~ /^([a-z]{2})(\d{3})([a-z]{2})(\d{4})$/)
+      return File.join($1, $2, $3, $4)
+    else
+      return nil
+    end
+  end
+
 end
