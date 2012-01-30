836e63e6874278f0c9361c16afd2dfa3892586b9
Christopher Jesudurai <jchris@stanford.edu>
2012-01-30 10:17:16 -0800

    Added initial iteration of gallery view for images

--- app/helpers/purl_helper.rb
+++ app/helpers/purl_helper.rb
@@ -18,12 +18,37 @@ module PurlHelper
     json_array = Array.new
     
     @purl.deliverable_files.each do |deliverable_file|
-      json_array.push(
-        "{ \"id\": \"" +  get_jp2_id(deliverable_file.filename.to_s) + "\"," + 
-           "\"label\": \"" + get_file_label(deliverable_file) + "\"," + 
-	         "\"width\": " + deliverable_file.width.to_s + "," + 
-	         "\"height\": " + deliverable_file.height.to_s + 
-	      "}")
+      id     = get_jp2_id(deliverable_file.filename.to_s)
+      width  = deliverable_file.width.to_s.empty? ? '0' : deliverable_file.width.to_s
+      height = deliverable_file.height.to_s.empty? ? '0' : deliverable_file.height.to_s
+      
+      if !id.nil? and !id.empty?
+        json_array.push(
+          "{ \"id\": \"" +  get_jp2_id(deliverable_file.filename.to_s) + "\"," + 
+             "\"label\": \"" + get_file_label(deliverable_file) + "\"," + 
+  	         "\"width\": " + width.to_s + "," + 
+  	         "\"height\": " + height.to_s + "," + 
+  	         "\"sequence\": " + deliverable_file.sequence.to_s + 
+  	      "}")
+  	  else 
+  	    if deliverable_file.sub_resources.length > 0 
+  	      deliverable_file.sub_resources.each do |img_file|
+            id     = get_jp2_id(img_file.filename.to_s)
+            width  = img_file.width.to_s.empty? ? '0' : img_file.width.to_s
+            height = img_file.height.to_s.empty? ? '0' : img_file.height.to_s
+
+            if !id.nil? and !id.empty?
+              json_array.push(
+                "{ \"id\": \"" +  get_jp2_id(img_file.filename.to_s) + "\"," + 
+                   "\"label\": \"" + get_file_label(img_file) + "\"," + 
+        	         "\"width\": " + width.to_s + "," + 
+        	         "\"height\": " + height.to_s + "," +  
+        	         "\"sequence\": " + deliverable_file.sequence.to_s + 
+        	      "}")
+        	  end      	        
+  	      end
+        end    
+	    end  
     end   
     
     json_array.join(',')
@@ -34,8 +59,12 @@ module PurlHelper
     img_id = get_jp2_id(deliverable_file.filename.to_s)
     base_url = deliverable_file.imagesvc.to_s
     
-    if (base_url.empty?)
+    if base_url.empty?
       base_url = STACKS_URL + "/image/" + @purl.pid + "/" + img_id
+    end
+    
+    if @purl.pid == "dm907qj6498"
+      base_url = base_url.gsub /\-test/i, ''
     end
     
     base_url
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,9 +1,11 @@
 
 class Resource
 
-  attr_accessor :objectId, :type, :mimetype, :size, :sequence, :description_label, :filename, :url, :imagesvc, :width, :height
+  attr_accessor :objectId, :type, :mimetype, :size, :sequence, :description_label, :filename, :url, :imagesvc, :width, :height, :sequence
 
   attr_accessor :shelve, :preserve, :deliver
+  
+  attr_accessor :sub_resources 
   
 end
   
