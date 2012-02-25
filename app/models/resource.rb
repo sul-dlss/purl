3d64f26f831f76c80260d8ab93a87c6a1b06e210
Renzo Sanchez-Silva <renzo@stanford.edu>
2012-02-24 16:51:08 -0800

    adding flipbook json service

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,7 @@
 
 class Resource
 
-  attr_accessor :objectId, :type, :mimetype, :size, :sequence, :description_label, :filename, :url, :imagesvc, :width, :height, :sequence
+  attr_accessor :objectId, :type, :mimetype, :size, :sequence, :description_label, :filename, :url, :imagesvc, :width, :height, :sequence, :levels
 
   attr_accessor :shelve, :preserve, :deliver
   
