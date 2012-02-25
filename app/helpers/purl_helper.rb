be81827241b37cef1da79a1454ac3afead07310d
Renzo Sanchez-Silva <renzo@stanford.edu>
2012-02-24 16:51:08 -0800

    adding flipbook json service

--- app/helpers/purl_helper.rb
+++ app/helpers/purl_helper.rb
@@ -1,3 +1,4 @@
+require 'lib/purl/util'
 
 module PurlHelper
 
@@ -9,80 +10,22 @@ module PurlHelper
 
   # get id from JP2 filename
   def get_jp2_id(filename)
-    filename = filename.gsub /\.jp2$/i, ''
-    filename
+    PurlUtils.get_jp2_id(filename)
   end
 
   # construct JSON array for delivering image objects
   def get_image_json_array
-    json_array = Array.new
-    
-    @purl.deliverable_files.each do |deliverable_file|
-      id     = get_jp2_id(deliverable_file.filename.to_s)
-      width  = deliverable_file.width.to_s.empty? ? '0' : deliverable_file.width.to_s
-      height = deliverable_file.height.to_s.empty? ? '0' : deliverable_file.height.to_s
-      
-      if !id.nil? and !id.empty?
-        json_array.push(
-          "{ \"id\": \"" +  get_jp2_id(deliverable_file.filename.to_s) + "\"," + 
-             "\"label\": \"" + get_file_label(deliverable_file) + "\"," + 
-  	         "\"width\": " + width.to_s + "," + 
-  	         "\"height\": " + height.to_s + "," + 
-  	         "\"sequence\": " + deliverable_file.sequence.to_s + 
-  	      "}")
-  	  else 
-  	    if deliverable_file.sub_resources.length > 0 
-  	      deliverable_file.sub_resources.each do |img_file|
-            id     = get_jp2_id(img_file.filename.to_s)
-            width  = img_file.width.to_s.empty? ? '0' : img_file.width.to_s
-            height = img_file.height.to_s.empty? ? '0' : img_file.height.to_s
-
-            if !id.nil? and !id.empty?
-              json_array.push(
-                "{ \"id\": \"" +  get_jp2_id(img_file.filename.to_s) + "\"," + 
-                   "\"label\": \"" + get_file_label(img_file) + "\"," + 
-        	         "\"width\": " + width.to_s + "," + 
-        	         "\"height\": " + height.to_s + "," +  
-        	         "\"sequence\": " + deliverable_file.sequence.to_s + 
-        	      "}")
-        	  end      	        
-  	      end
-        end    
-	    end  
-    end   
-    
-    json_array.join(',')
+    PurlUtils.get_image_json_array(@purl.deliverable_files)
   end
 
   # construct base URL using stacks URL
   def get_img_base_url(deliverable_file)
-    img_id = get_jp2_id(deliverable_file.filename.to_s)
-    base_url = deliverable_file.imagesvc.to_s
-    
-    if base_url.empty?
-      base_url = STACKS_URL + "/image/" + @purl.pid + "/" + img_id
-    end
-    
-    if @purl.pid == "dm907qj6498"
-      base_url = base_url.gsub /\-test/i, ''
-    end
-    
-    base_url
+    PurlUtils.get_img_base_url(@purl.pid, STACKS_URL, deliverable_file)
   end
 
   # get file label (if available) or jp2 id
   def get_file_label(deliverable_file)
-    label = get_jp2_id(deliverable_file.filename.to_s)
-    
-    if (!deliverable_file.description_label.nil? && !deliverable_file.description_label.empty?)
-      label = deliverable_file.description_label.to_s
-    end
-    
-    if label.length > 45
-      label = label[0 .. 44] + '...'
-    end
-    
-    label
+    PurlUtils.get_file_label(deliverable_file)
   end
   
   # get field value
