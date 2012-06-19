5929189beec2f29a485f9bb12f16116275d34148
Christopher Jesudurai <jchris@stanford.edu>
2012-06-19 14:22:27 -0700

    Added dor-rights-auth gem for WebAuth

--- Gemfile
+++ Gemfile
@@ -2,9 +2,10 @@ source :rubygems
 source "http://sulair-rails-dev.stanford.edu"
 
 gem 'rails', '2.3.4'
-
 gem 'rake'
+
 #gem 'rack-webauth', :git => "git://github.com/sul-dlss/rack-webauth.git"
 
 gem 'mongrel' # or thin
 gem 'htmlentities'
+gem 'dor-rights-auth'
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,11 +1,7 @@
-
 class Resource
-
-  attr_accessor :objectId, :type, :mimetype, :size, :sequence, :description_label, :filename, :url, :imagesvc, :width, :height, :sequence, :levels, :access_world, :access_stanford
-
-  attr_accessor :shelve, :preserve, :deliver
-  
-  attr_accessor :sub_resources 
-  
+  attr_accessor :objectId, :type, :mimetype, :size, :sequence, :description_label, :filename, :url, :imagesvc, :width, :height, :sequence, :levels
+  attr_accessor :rights_world, :rights_world_rule, :rights_stanford, :rights_stanford_rule
+  attr_accessor :shelve, :preserve, :deliver  
+  attr_accessor :sub_resources   
 end
   
