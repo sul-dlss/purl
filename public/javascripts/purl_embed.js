58469c0393e3bb5752da67eda01f8e0c295491a5
Christopher Jesudurai <jchris@stanford.edu>
2012-11-28 11:16:00 -0800

    Added download links for WebAuth-ed links in jQuery embed plugin

--- public/javascripts/purl_embed.js
+++ public/javascripts/purl_embed.js
@@ -319,7 +319,7 @@ var purlEmbed = (function(data, pid, stacksURL, inputSequence, inputSize) {
           html = "";
           html = "<span class=\"pe-" + size + "-img-viewer\" href=\"" + url + "\" class=\"su\">" + size + "</span>" +
               "<span class=\"pe-v-nav-img-size\"> (" + getDimensionsForSizeWxH(index, size)  + ")</span>" +
-              "<a class=\"pe-" + size + "-img-viewer-download pe-download-icon\"></a>" +
+              "<a href=\"" + getDownloadLink(index, size) + "\" class=\"pe-" + size + "-img-viewer-download pe-download-icon\"></a>" +
               "<img src=\"" + peServerURL + "/images/icon-stanford-only.png\" class=\"pe-icon-stanford-only\" alt=\"Stanford Only\" title=\"Stanford Only\"/>";
 
           if (size !== "thumb") {
