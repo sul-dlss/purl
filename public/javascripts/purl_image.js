575e79ced83042f0e7987bbfabe43bf7f856f4ae
Christopher Jesudurai <jchris@stanford.edu>
2013-03-28 16:28:53 -0700

    Added collapsible footer to non-image pages

--- public/javascripts/purl_image.js
+++ public/javascripts/purl_image.js
@@ -82,17 +82,6 @@ $(document).ready(function() {
     $('ol#toc').hide();
   });
 
-  $('#footer-show-hide').toggle(
-    function() {
-      $('#footer-contents').show();
-      $('#footer-show-hide').css('background-image', 'url("/images/icon-img-footer-hide.png")');
-    },
-    function () {
-      $('#footer-contents').hide();
-      $('#footer-show-hide').css('background-image', 'url("/images/icon-img-footer-show.png")');
-    }
-  );
-
   setupGalleryPageNavigation(1);
   setURLSuffix();
 });
