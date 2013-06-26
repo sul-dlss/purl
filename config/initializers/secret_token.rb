51ac302310773e6cbed2981d7d68601cc8f3637e
Jonathan Deering <jdeering@stanford.edu>
2013-06-26 16:30:12 -0700

    make flipbook and gallery view work and add fixtures

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purl::Application.config.secret_token = '37aa3fb1dad2399670fc0557d33c016ff9f12e46eb999b5d5a2c886dae26bca4ac91aad6289f8349629c800894476672f344050ec578aef563fe5f192ab3c889'
