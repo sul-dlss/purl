92a925441a95fead84d080d7aab16494b31202e6
Christopher Jesudurai <jchris@stanford.edu>
2013-08-15 17:18:21 -0700

    Added SUL elements to foooter, plus fixes

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  $('#footer-show-hide').toggle(
+  $('.footer-show-hide').toggle(
     function() {
-      $('#footer-contents').show();
-      $('#footer-show-hide').css('background-image', 'url("/images/icon-img-footer-hide.png")');
+      $('.footer-contents').show();
+      $('.footer-show-hide').css('background-image', 'url("/images/icon-img-footer-hide.png")');
     },
     function () {
-      $('#footer-contents').hide();
-      $('#footer-show-hide').css('background-image', 'url("/images/icon-img-footer-show.png")');
+      $('.footer-contents').hide();
+      $('.footer-show-hide').css('background-image', 'url("/images/icon-img-footer-show.png")');
     }
   );
 });
