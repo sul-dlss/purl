1ec25e5b7a68f7f0b597c1fb4090e6737ee24bd9
Jonathan Deering <jdeering@stanford.edu>
2013-08-28 10:51:54 -0700

    fix app name in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-PurlRails3::Application.configure do
+Purl::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
