407341035456f0478572c60687fdbc7bd985a313
Chris Beer <cabeer@stanford.edu>
2014-07-30 11:49:42 -0700

    Update to Capistrano 3

--- config/deploy/testing.rb
+++ config/deploy/testing.rb
@@ -1,9 +1,4 @@
+server 'sul-purl-test.stanford.edu', user: 'lyberadmin', roles: %w{web db app}
+
+Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, "test"
-set :deployment_host, "sul-purl-test.stanford.edu"
-set :bundle_without, [:deployment,:development,:production]
-set :scm, :none
-set :repository, "../"
-set :deploy_via, :copy
-role :web, deployment_host
-role :app, deployment_host
-role :db,  deployment_host, :primary => true
