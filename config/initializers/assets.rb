93c9d8b92d92ab767cb72b9c11902765b4f1a69f
Chris Beer <cabeer@stanford.edu>
2015-07-22 08:57:55 -0700

    Set the asset host, if available

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( site.css purl_image.css purl_embed.css purl_flipbook.css purl_book.js purl_embed_jquery_plugin.js purl_embed.js purl_flipbook.js purl_image.js)
+
+Rails.application.config.asset_host = Settings.asset_host unless Settings.asset_host.blank?
