f1efa63982a53d217cdfb86638d59846648c0e5c
Chris Beer <cabeer@stanford.edu>
2015-07-28 16:26:46 -0700

    Remove unused test/ tests

--- test/unit/purl_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PurlTest < ActiveSupport::TestCase
-  test 'loading from the doc cache' do
-    item = Purl.new('bb157hs6068')
-    assert !item.nil?
-    assert item.ready?
-  end
-end
