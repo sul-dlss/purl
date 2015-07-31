7f9960f184e6e9486698d73785b2c1a255abfc06
Chris Beer <cabeer@stanford.edu>
2015-07-31 12:21:54 -0700

    Refactor PurlResource to use concerns

--- app/models/purl_resource.rb
+++ app/models/purl_resource.rb
@@ -8,6 +8,14 @@ class PurlResource
   class DruidNotValid < StandardError; end
   class ObjectNotReady < StandardError; end
 
+  def self.find(id)
+    fail DruidNotValid, id unless Dor::Util.validate_druid(id)
+
+    PurlResource.new(id: id).tap do |obj|
+      fail ObjectNotReady, id unless obj.ready?
+    end
+  end
+
   # rubocop:disable Metrics/MethodLength, Style/PredicateName
   def self.has_resource(options)
     options.each do |key, value|
@@ -18,57 +26,40 @@ class PurlResource
       end
 
       define_method "#{key}_body" do
-        send("#{key}_resource").body if send("#{key}?")
+        send("#{key}_resource").body if send("#{key}_resource").success?
       end
 
       define_method "#{key}?" do
-        send("#{key}_resource").success?
+        send("#{key}_body").present?
       end
     end
   end
   # rubocop:enable Metrics/MethodLength, Style/PredicateName
 
-  def response_cache
-    @response_cache ||= {}
-  end
-
   has_resource mods: Settings.purl_resource.mods
   has_resource public_xml: Settings.purl_resource.public_xml
   has_resource iiif_manifest: Settings.purl_resource.iiif_manifest
-
-  def self.find(id)
-    fail DruidNotValid, id unless Dor::Util.validate_druid(id)
-
-    PurlResource.new(id: id).tap do |obj|
-      fail ObjectNotReady, id unless obj.ready?
-    end
-  end
-
-  def title
-    if mods?
-      Array.wrap(mods.title).join(' -- ')
-    else
-      public_xml.title
-    end
-  end
-
-  def description
-    @description ||= begin
-      abstract = mods.abstract.detect { |a| a.respond_to? :values }
-      if abstract
-        abstract.values.join
-      else
-        ''
-      end
-    end if mods?
-  end
 
   def ready?
     public_xml?
   end
 
-  def mods_display_object
-    @mods_display_object ||= ModsDisplayObject.new(mods_body)
+  def public_xml_document
+    @public_xml_document ||= Nokogiri::XML(public_xml_body)
+  end
+
+  def public_xml
+    @public_xml ||= PublicXml.new(public_xml_document)
+  end
+
+  delegate :rights_metadata, to: :public_xml
+
+  def content_metadata
+    @content_metadata ||= ContentMetadata.new(public_xml.content_metadata)
+  end
+
+  def rights
+    @rights ||= RightsMetadata.new(rights_metadata)
   end
 
   def mods
@@ -78,139 +69,160 @@ class PurlResource
     end
   end
 
-  def public_xml_document
-    @public_xml_document ||= Nokogiri::XML(public_xml_body)
-  end
-
-  def mods_document
-    @mods_document ||= Nokogiri::XML(mods_body)
-  end
-
-  def rights
-    @rights ||= RightsMetadata.new(rights_metadata)
-  end
-
-  delegate :rights_metadata, to: :public_xml
-
-  def content_metadata
-    @content_metadata ||= ContentMetadata.new(public_xml.content_metadata)
-  end
-
-  def public_xml
-    @public_xml ||= PublicXml.new(public_xml_document)
-  end
-
-  def type
-    @type ||= content_metadata.type
-  end
-
-  def flipbook?
-    type =~ /Book|Manuscript/i
+  def iiif_manifest
+    return {} unless iiif_manifest?
+    @iiif_manifest ||= JSON.parse(iiif_manifest_body)
   end
 
   def flipbook
     @flipbook ||= Flipbook.new(self)
   end
 
-  def attributes
-    { druid: id, druid_tree: druid_tree }
-  end
+  concerning :Metadata do
+    def title
+      if mods?
+        Array.wrap(mods.title).join(' -- ')
+      else
+        public_xml.title
+      end
+    end
 
-  def druid_tree
-    Dor::Util.create_pair_tree(druid) || druid
-  end
+    def description
+      @description ||= begin
+        abstract = mods.abstract.detect { |a| a.respond_to? :values }
+        if abstract
+          abstract.values.join
+        else
+          ''
+        end
+      end if mods?
+    end
 
-  def cache_key
-    "purl_resource/druid:#{id}"
-  end
+    def type
+      @type ||= content_metadata.type
+    end
 
-  def cache_resource(key, &block)
-    Rails.cache.fetch("#{cache_key}/#{key}", expires_in: Settings.resource_cache.lifetime, &block)
-  end
+    def license
+      @license ||= License.new(code: license_code, text: license_text)
+    end
 
-  def fetch_resource(value)
-    url_or_path = value % attributes
+    def license?
+      license_code.present? || license_text.present?
+    end
 
-    case url_or_path
-    when /^http/
-      Hurley.get(url_or_path)
-    else
-      DocumentCacheResource.new(url_or_path)
+    def license_code
+      type, code = rights.machine_readable_license
+
+      "#{type}-#{code}" if type.present? && code.present?
+    end
+
+    def license_text
+      rights.license_statement
+    end
+
+    def copyright?
+      copyright.present?
+    end
+
+    def copyright
+      rights.copyright_statement
+    end
+
+    def use_and_reproduction?
+      use_and_reproduction.present?
+    end
+
+    def use_and_reproduction
+      rights.use_and_reproduction_statement
+    end
+
+    def catalog_key
+      @catalog_key ||= begin
+        key = public_xml_document.xpath('/publicObject/identityMetadata/otherId[@name="catkey"]/text()').to_s
+
+        key if key.present?
+      end
+    end
+
+    def released_to?(key)
+      release = public_xml_document.xpath("/publicObject/releaseData/release[@to='#{key}']/text()").to_s
+
+      release == 'true'
+    end
+
+    def representative_thumbnail?
+      representative_thumbnail.present?
+    end
+
+    def representative_thumbnail
+      iiif_manifest.fetch('thumbnail', {})['@id']
+    end
+
+    def flipbook?
+      type =~ /Book|Manuscript/i
     end
   end
 
-  def license
-    @license ||= License.new(code: license_code, text: license_text)
-  end
+  concerning :Caching do
+    def cache_key
+      "purl_resource/druid:#{id}"
+    end
 
-  def license?
-    license_code.present? || license_text.present?
-  end
-
-  def license_code
-    type, code = rights.machine_readable_license
-
-    "#{type}-#{code}" if type.present? && code.present?
-  end
-
-  def license_text
-    rights.license_statement
-  end
-
-  def copyright?
-    copyright.present?
-  end
-
-  def copyright
-    rights.copyright_statement
-  end
-
-  def use_and_reproduction?
-    use_and_reproduction.present?
-  end
-
-  def use_and_reproduction
-    rights.use_and_reproduction_statement
-  end
-
-  def catalog_key
-    @catalog_key ||= begin
-      key = public_xml_document.xpath('/publicObject/identityMetadata/otherId[@name="catkey"]/text()').to_s
-
-      key if key.present?
+    def updated_at
+      if public_xml_resource.respond_to? :updated_at
+        public_xml_resource.updated_at
+      elsif public_xml_resource.respond_to?(:header) && public_xml_resource.header[:last_modified].present?
+        public_xml_resource.header[:last_modified]
+      else
+        Time.zone.now
+      end
     end
   end
 
-  def released_to?(key)
-    release = public_xml_document.xpath("/publicObject/releaseData/release[@to='#{key}']/text()").to_s
+  concerning :ActiveModelness do
+    def attributes
+      { druid: id, druid_tree: druid_tree }
+    end
 
-    release == 'true'
-  end
+    def persisted?
+      true
+    end
 
-  def persisted?
-    true
-  end
+    private
 
-  def updated_at
-    if public_xml_resource.respond_to? :updated_at
-      public_xml_resource.updated_at
-    elsif public_xml_resource.respond_to?(:header) && public_xml_resource.header[:last_modified].present?
-      public_xml_resource.header[:last_modified]
-    else
-      Time.zone.now
+    def druid_tree
+      Dor::Util.create_pair_tree(druid) || druid
     end
   end
 
-  def iiif_manifest
-    return {} unless iiif_manifest?
-    @iiif_manifest ||= JSON.parse(iiif_manifest_body)
+  concerning :Fetching do
+    def cache_resource(key, &block)
+      Rails.cache.fetch("#{cache_key}/#{key}", expires_in: Settings.resource_cache.lifetime, &block)
+    end
+
+    def response_cache
+      @response_cache ||= {}
+    end
+
+    def fetch_resource(value)
+      url_or_path = value % attributes
+
+      case url_or_path
+      when /^http/
+        Hurley.get(url_or_path)
+      else
+        DocumentCacheResource.new(url_or_path)
+      end
+    end
   end
 
-  def representative_thumbnail?
-    representative_thumbnail.present?
+  private
+
+  def mods_display_object
+    @mods_display_object ||= ModsDisplayObject.new(mods_body)
   end
 
-  def representative_thumbnail
-    iiif_manifest.fetch('thumbnail', {})['@id']
+  def mods_document
+    @mods_document ||= Nokogiri::XML(mods_body)
   end
 end
