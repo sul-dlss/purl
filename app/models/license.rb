d903f32ef34a5787f26e33667c2914361f856018
Chris Beer <cabeer@stanford.edu>
2015-09-29 18:28:17 -0700

    Use ruby 1.9 compatible hashes for license information

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,50 +1,50 @@
 class License
   LICENSES = {
     cc: {
-      'by': {
+      :'by' => {
         link: 'http://creativecommons.org/licenses/by/3.0/',
         desc: 'This work is licensed under a Creative Commons Attribution 3.0 Unported License'
       },
-      'by-sa': {
+      :'by-sa' => {
         link: 'http://creativecommons.org/licenses/by-sa/3.0/',
         desc: 'This work is licensed under a Creative Commons Attribution-Share Alike 3.0 Unported License'
       },
-      'by-nd': {
+      :'by-nd' => {
         link: 'http://creativecommons.org/licenses/by-nd/3.0/',
         desc: 'This work is licensed under a Creative Commons Attribution-No Derivative Works 3.0 Unported License'
       },
-      'by-nc': {
+      :'by-nc' => {
         link: 'http://creativecommons.org/licenses/by-nc/3.0/',
         desc: 'This work is licensed under a Creative Commons Attribution-Noncommercial 3.0 Unported License'
       },
-      'by-nc-sa': {
+      :'by-nc-sa' => {
         link: 'http://creativecommons.org/licenses/by-nc-sa/3.0/',
         desc: 'This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License'
       },
-      'by-nc-nd': {
+      :'by-nc-nd' => {
         link: 'http://creativecommons.org/licenses/by-nc-nd/3.0/',
         desc: 'This work is licensed under a Creative Commons Attribution-Noncommercial-No Derivative Works 3.0 Unported License'
       },
-      'pdm': {
+      :'pdm' => {
         link: 'http://creativecommons.org/publicdomain/mark/1.0/',
         desc: 'This work is in the public domain per Creative Commons Public Domain Mark 1.0'
       }
     },
 
     odc: {
-      'odc-pddl': {
+      :'odc-pddl' => {
         link: 'http://opendatacommons.org/licenses/pddl/',
         desc: 'This work is licensed under a Open Data Commons Public Domain Dedication and License (PDDL)'
       },
-      'pddl': {
+      :'pddl' => {
         link: 'http://opendatacommons.org/licenses/pddl/',
         desc: 'This work is licensed under a Open Data Commons Public Domain Dedication and License (PDDL)'
       },
-      'odc-by': {
+      :'odc-by' => {
         link: 'http://opendatacommons.org/licenses/by/',
         desc: 'This work is licensed under a Open Data Commons Attribution License'
       },
-      'odc-odbl': {
+      :'odc-odbl' => {
         link: 'http://opendatacommons.org/licenses/odbl/',
         desc: 'This work is licensed under a Open Data Commons Open Database License (ODbL)'
       }
