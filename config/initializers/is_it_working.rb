7a63b90f413a18ca58631c006ac9d3d5b46bff03
Chris Beer <cabeer@stanford.edu>
2015-10-06 09:01:58 -0700

    Use memcache-backed cache store in production

--- config/initializers/is_it_working.rb
+++ config/initializers/is_it_working.rb
@@ -1,3 +1,6 @@
 Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
   h.check :directory, path: Settings.document_cache_root
+
+  # Check the memcache servers used by Rails.cache if using the DalliStore implementation
+  h.check :dalli, cache: Rails.cache if defined?(ActiveSupport::Cache::DalliStore) && Rails.cache.is_a?(ActiveSupport::Cache::DalliStore)
 end
