5482554b09b4faa92baed19d886af4e35596d9e9
Chris Beer <cabeer@stanford.edu>
2015-10-22 13:07:21 -0700

    Use the purl user for deploys

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,4 +1,4 @@
-server 'sul-purl-dev.stanford.edu', user: 'lyberadmin', roles: %w(web db app)
+server 'sul-purl-dev.stanford.edu', user: 'purl', roles: %w(web db app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'development'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # server 'sul-purl-prod.stanford.edu', user: 'lyberadmin', roles: %w(web db app)
-server 'sul-purl-prod-b.stanford.edu', user: 'lyberadmin', roles: %w(web db app)
+server 'sul-purl-prod-b.stanford.edu', user: 'purl', roles: %w(web db app)
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
