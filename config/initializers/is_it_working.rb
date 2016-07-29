0caac7c1c14171521448d52fe5705d511276edb3
Naomi Dushay <ndushay@stanford.edu>
2016-07-29 16:54:54 -0700

    add checks to is_it_working

--- config/initializers/is_it_working.rb
+++ config/initializers/is_it_working.rb
@@ -3,4 +3,10 @@ Rails.configuration.middleware.use(IsItWorking::Handler) do |h|
 
   # Check the memcache servers used by Rails.cache if using the DalliStore implementation
   h.check :dalli, cache: Rails.cache if defined?(ActiveSupport::Cache::DalliStore) && Rails.cache.is_a?(ActiveSupport::Cache::DalliStore)
+
+  h.check :url, get: Settings.stacks.url
+  h.check :url, get: Settings.stacks.iiif_profile
+
+  h.check :url, get: Settings.flipbook.url
+  h.check :url, get: 'https://embed.stanford.edu'
 end
