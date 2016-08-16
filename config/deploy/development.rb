3fcf12cd2df708453f2e2f39e3b822f9525a74ef
Naomi Dushay <ndushay@stanford.edu>
2016-08-15 17:05:48 -0700

    deploy to dev without deployment gems

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -3,4 +3,4 @@ server 'sul-purl-dev.stanford.edu', user: 'purl', roles: %w(web db app)
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'development'
 
-set :bundle_without, %w(test).join(' ')
+set :bundle_without, %w{deployment test}.join(' ')
