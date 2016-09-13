0ddd02191d6403c94209909329aa911828c0a471
Naomi Dushay <ndushay@stanford.edu>
2016-09-13 16:21:16 -0700

    deploy: set log level to info, not debug

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :deploy_to, '/opt/app/purl/purl'
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :info
 
 # Default value for :pty is false
 # set :pty, true
