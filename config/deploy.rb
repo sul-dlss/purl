3da21d765848f4730dd399018965000bd40abc28
Chris Beer <chris@cbeer.info>
2017-01-06 09:15:24 -0800

    Remove sitemap generation; fixes #105

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,10 +20,10 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w(config/secrets.yml config/database.yml config/honeybadger.yml config/newrelic.yml public/robots.txt public/sitemap.xml)
+set :linked_files, %w(config/secrets.yml config/database.yml config/honeybadger.yml config/newrelic.yml public/robots.txt)
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w(config/settings log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sitemaps public/system)
+set :linked_dirs, %w(config/settings log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
