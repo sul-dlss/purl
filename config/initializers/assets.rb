6902fa2002474f957fa0a0eaecc0b033b99f6ec8
Chris Beer <cabeer@stanford.edu>
2017-09-27 07:55:06 -0700

    Update to Rails 5.1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,5 +5,13 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.asset_host = Settings.asset_host unless Settings.asset_host.blank?
 
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
 Rails.application.config.assets.precompile += %w( print.css )
 Rails.application.config.assets.precompile += %w( fileicons/*.png )
