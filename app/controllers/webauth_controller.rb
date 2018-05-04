d7ed81e532c9efc03dbcc81edec547b64582d704
Chris Beer <cabeer@stanford.edu>
2018-05-04 07:38:21 -0700

    Use Rails 5's redirect_back instead of using the old way that no longer works

--- app/controllers/webauth_controller.rb
+++ app/controllers/webauth_controller.rb
@@ -2,11 +2,11 @@ class WebauthController < ApplicationController
   def login
     flash[:success] = 'You have been successfully logged in.'
 
-    redirect_to params[:referrer] || :back
+    redirect_back fallback_location: root_url
   end
 
   def logout
     flash[:notice] = 'You have been successfully logged out.'
-    redirect_to params[:referrer] || :back
+    redirect_back fallback_location: root_url
   end
 end
