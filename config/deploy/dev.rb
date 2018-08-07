e61eb3b6516c7f0c77af736d17ee4c0ff8df8b45
Jack Reed <phillipjreed@gmail.com>
2018-08-07 08:35:41 -0600

    remove pending decommission env

--- config/deploy/dev.rb
+++
@@ -1,6 +0,0 @@
-server 'sul-purl-dev.stanford.edu', user: 'purl', roles: %w(web db app)
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'development'
-
-set :bundle_without, %w{deployment test}.join(' ')
