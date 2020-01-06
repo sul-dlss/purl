3550db1bc321950e7fdcb0db9dcbb77b25b685b2
Jack Reed <phillipjreed@gmail.com>
2020-01-06 07:37:59 -0700

    Appease rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ require 'selenium-webdriver'
 Capybara.javascript_driver = :selenium_chrome_headless
 
 # Auto require all files in spec/support.
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
