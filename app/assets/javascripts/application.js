70fe1c537a192d4239d63883b6b176f2cfdc31c0
Chris Beer <cabeer@stanford.edu>
2020-04-10 07:50:52 -0700

    Update to Bootstrap 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/transition
+//= require bootstrap/util
 //= require bootstrap/collapse
 //= require bootstrap/dropdown
 //= require bootstrap/alert
