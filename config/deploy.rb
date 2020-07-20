b668f021fa0e2cb5f464f0ecd9ee3f75ffd27fd9
Jack Reed <phillipjreed@gmail.com>
2020-07-20 07:40:38 -0600

    Ask for branch to deploy unless env DEPLOY is present to consolidate deploy practice

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'purl'
 set :repo_url, 'https://github.com/sul-dlss/purl.git'
 
 # Default branch is :master
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call unless ENV['DEPLOY']
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/opt/app/purl/purl'
