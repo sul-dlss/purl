f22b94dcfe572086ce028d17c1c5fb7fbb21ef38
Chris Beer <chris@cbeer.info>
2021-03-16 14:03:34 -0700

    Display the as-rendered-by-purl content for the preview

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -3,5 +3,18 @@ class PreviewController < ApplicationController
 
   def show
     @mods = ModsDisplayObject.new(params[:mods])
+    @mods = @mods.render_mods_display(@mods)
+    @purl = @document = OpenStruct.new(
+      mods?: true,
+      mods: @mods,
+      title: Array.wrap(@mods.title).join(' -- ')
+    )
+
+    @purl.define_singleton_method(:released_to?) do |*args|
+    end
+  end
+
+  def self.local_prefixes
+    [controller_path, '/purl']
   end
 end
