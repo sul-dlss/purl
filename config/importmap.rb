800a74053621323c185c98324ce0d1bb548004bc
Justin Coyne <jcoyne@justincoyne.com>
2023-03-21 11:42:43 -0500

    Remove jQuery.XDomainRequest
    This was a polyfill for IE8-9, which we no longer support. See https://github.com/MoonScript/jQuery-ajaxTransport-XDomainRequest

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,6 @@ pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.1/dist/jquery.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
 pin 'jquery.oembed'
-pin 'jQuery.XDomainRequest'
 pin 'analytics'
 pin 'feedback_form'
 pin 'purl_embed'
