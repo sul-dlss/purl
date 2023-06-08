fe420f46953e76440419f232a5242bfe4eb437ec
Justin Coyne <jcoyne@justincoyne.com>
2023-06-08 08:24:26 -0500

    Make header matching more complex

--- spec/model/accept_header_constraint_spec.rb
+++ spec/model/accept_header_constraint_spec.rb
@@ -2,20 +2,42 @@ require 'rails_helper'
 
 RSpec.describe AcceptHeaderConstraint do
   subject(:instance) { described_class.new }
-  let(:v3_request) do
-    double(
-      'request',
-      headers: { 'Accept' => 'application/ld+json;profile="http://iiif.io/api/presentation/3/context.json"' }
-    )
-  end
-  let(:default_request) do
-    double('request', headers: {})
-  end
+
   describe '#matches?' do
-    context 'with proper Accept headers' do
-      it { expect(instance.matches?(v3_request)).to be_truthy }
+    context 'with Accept header that has v3 profile' do
+      let(:request) do
+        double(
+          'request',
+          headers: { 'Accept' => 'application/ld+json;profile="http://iiif.io/api/presentation/3/context.json"' }
+        )
+      end
+      it { expect(instance.matches?(request)).to be_truthy }
     end
+
+    context 'with Accept header that has v3 profile and weight' do
+      let(:request) do
+        double(
+          'request',
+          headers: { 'Accept' => 'application/ld+json;q=0.5;profile="http://iiif.io/api/presentation/3/context.json"' }
+        )
+      end
+      it { expect(instance.matches?(request)).to be_truthy }
+    end
+
+    context 'with Accept header that has v3 and v2 profile and weight' do
+      let(:request) do
+        double(
+          'request',
+          headers: { 'Accept' => 'application/ld+json;q=0.5;profile="http://iiif.io/api/presentation/2/context.json", application/ld+json;profile="http://iiif.io/api/presentation/3/context.json"' }
+        )
+      end
+      it { expect(instance.matches?(request)).to be_truthy }
+    end
+
     context 'without proper Accept headers' do
+      let(:default_request) do
+        double('request', headers: {})
+      end
       it { expect(instance.matches?(default_request)).to be_falsy }
     end
   end
