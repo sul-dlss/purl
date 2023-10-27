43dd96608b11a117a6570b5c96445c84dd73e23c
Justin Littman <justinlittman@stanford.edu>
2023-10-27 11:03:01 -0400

    Minor view refactoring

--- spec/views/purl/_find_it.html.erb_spec.rb
+++ spec/views/purl/_find_it.html.erb_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'purl/_find_it' do
-  before do
-    assign(:purl, purl)
-  end
-
   context 'Content-type geo' do
     let(:purl) { PurlResource.new(id: 'cg357zz0321') }
+
     it 'displays a View in EarthWorks link' do
-      render
+      render 'purl/find_it', document: purl
       expect(rendered).to have_css 'a[href="https://earthworks.stanford.edu/catalog/stanford-cg357zz0321"]', text: 'View in EarthWorks'
     end
   end
+
   context 'Released to SearchWorks' do
     let(:purl) { PurlResource.new(id: 'bf973rp9392') }
+
     it 'displays a View in SearchWorks link' do
-      render
+      render 'purl/find_it', document: purl
       expect(rendered).to have_css 'a[href="https://searchworks.stanford.edu/view/bf973rp9392"]', text: 'View in SearchWorks'
     end
   end
