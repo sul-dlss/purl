390a74d5e6ee80bf9a42a99edbc05fd291fb7a5c
Justin Littman <justinlittman@stanford.edu>
2023-11-01 12:49:07 -0400

    Selectively import ace editor.
    refs #757

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,8 @@ pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/inde
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/preview_controllers", under: "preview_controllers"
+# Preview is a separate application to avoid loading ace-builds for every page
+pin 'preview_application'
 pin 'feedback_form'
 pin "ace-builds", to: "https://ga.jspm.io/npm:ace-builds@1.16.0/src-noconflict/ace.js"
