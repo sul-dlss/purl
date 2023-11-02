d70b4228711e75c9fac13740b15d10352902e84c
Justin Littman <justinlittman@stanford.edu>
2023-11-02 08:03:14 -0400

    Remove feedback form from importmap.

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/preview_controllers", under: "preview_controllers"
 # Preview is a separate application to avoid loading ace-builds for every page
 pin 'preview_application'
-pin 'feedback_form'
 pin "ace-builds", to: "https://ga.jspm.io/npm:ace-builds@1.16.0/src-noconflict/ace.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
