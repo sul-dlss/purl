6879950a086cc7f466df1cacfca8315aaae6cb7c
Cory Lown <corylown@stanford.edu>
2023-11-13 11:32:56 -0500

    Require action_cable/engine

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "active_model/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
+require "action_cable/engine"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
