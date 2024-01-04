5ca3888f3666a728b59ec97304f95ba1863a9126
Justin Coyne <jcoyne@justincoyne.com>
2024-01-04 15:05:27 -0600

    Remove blank IIIF manifest metadata

--- app/models/iiif_presentation_manifest.rb
+++ app/models/iiif_presentation_manifest.rb
@@ -285,13 +285,15 @@ class IiifPresentationManifest
   def dc_to_iiif_metadata
     @dc_to_iiif_metadata ||= begin
       all_dc_nodes = public_xml_document.xpath '//oai_dc:dc/*', 'oai_dc' => OAI_DC_SCHEMA
-      metadata = all_dc_nodes.map { |dc_node| iiif_key_value(dc_node.name.upcase_first, dc_node.text) }
+      metadata = all_dc_nodes.filter_map { |dc_node| iiif_key_value(dc_node.name.upcase_first, dc_node.text) }
       metadata += public_xml_document.xpath('/publicObject/@published').map { |node| iiif_key_value('PublishDate', node.text) } # add published date
       metadata
     end
   end
 
   def iiif_key_value(label, value)
+    return if value.blank?
+
     { 'label' => label, 'value' => value }
   end
 
