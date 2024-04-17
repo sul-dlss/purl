37cfda1291bbcace14482f196abf7255a6db612a
Justin Coyne <jcoyne@justincoyne.com>
2024-04-17 13:55:01 -0500

    Extract annotation_page request spec to a separate file

---
+++ spec/requests/iiif_annotation_page_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'IIIF Annotation Page' do
+  context 'when using v3' do
+    it 'renders the json for manifest' do
+      get '/bc854fy5899/iiif3/annotation_page/bc854fy5899_1'
+      expect(response.parsed_body['items'].first['motivation']).to eq('painting')
+    end
+
+    it 'renders nil for a non-manifest' do
+      get '/wp335yr5649/iiif3/annotation_page/bc854fy5899_fdsa'
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+end
