14e9cf506f1d5888410b07a7bc4ab7c231a06d52
Justin Coyne <jcoyne@justincoyne.com>
2024-06-11 16:28:14 -0500

    Avoid unnecessary tbody tags
    These cause accessibility errors

--- app/components/table_component.rb
+++ app/components/table_component.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
 class TableComponent < ViewComponent::Base
-  def initialize(label_id:, with_body: true)
+  def initialize(label_id:)
     @label_id = label_id
-    @with_body = with_body
     super()
   end
 
   attr_reader :label_id
-
-  def with_body?
-    @with_body
-  end
 end
