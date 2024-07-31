45fee38d4e3a577125f59ee7b6e125d6032fc1a7
Michael J. Giarlo <mjgiarlo@stanford.edu>
2024-07-31 13:17:52 -0700

    Integrate with version-aware embed service
    Connects with sul-dlss/sul-embed#2201

--- spec/views/purl/_embed.html.erb_spec.rb
+++ spec/views/purl/_embed.html.erb_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'purl/_embed' do
-  before do
-    assign(:purl, purl)
-  end
+  before { assign(:purl, purl) }
 
   let(:purl) { PurlResource.new(id: 'xyz') }
 
@@ -12,12 +10,34 @@ RSpec.describe 'purl/_embed' do
     expect(rendered).to have_css '.purl-embed-viewer'
     embed = Nokogiri::HTML(rendered).css('.purl-embed-viewer')
     expect(embed.attr('data-oembed-provider').to_s).to eq 'https://embed.stanford.edu/embed.json?hide_title=true&hide_metadata=true'
+    expect(embed.attr('href').to_s).to eq('https://purl.stanford.edu/xyz')
   end
 
-  it 'display a non-javascript fallback' do
+  it 'displays a non-javascript fallback' do
     render
     expect(rendered).to have_css 'noscript iframe'
     iframe = Nokogiri::HTML(rendered).xpath('//iframe')
     expect(iframe.attr('src').to_s).to eq 'https://embed.stanford.edu/iframe/?url=https%3A%2F%2Fpurl.stanford.edu%2Fxyz'
+  end
+
+  context 'when a version is specified' do
+    before { assign(:version, version) }
+
+    let(:version) { PurlVersion.new(id: 'xyz', head: true, withdrawn: false, version_id: 2) }
+
+    it 'displays a purl embed viewer' do
+      render
+      expect(rendered).to have_css '.purl-embed-viewer'
+      embed = Nokogiri::HTML(rendered).css('.purl-embed-viewer')
+      expect(embed.attr('data-oembed-provider').to_s).to eq 'https://embed.stanford.edu/embed.json?hide_title=true&hide_metadata=true'
+      expect(embed.attr('href').to_s).to eq('https://purl.stanford.edu/xyz/v2')
+    end
+
+    it 'displays a non-javascript fallback' do
+      render
+      expect(rendered).to have_css 'noscript iframe'
+      iframe = Nokogiri::HTML(rendered).xpath('//iframe')
+      expect(iframe.attr('src').to_s).to eq 'https://embed.stanford.edu/iframe/?url=https%3A%2F%2Fpurl.stanford.edu%2Fxyz%2Fv2'
+    end
   end
 end
