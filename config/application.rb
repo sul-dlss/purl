0c393f49e15942c3568d5fa2c611373b1b0074b5
Justin Coyne <jcoyne@justincoyne.com>
2024-08-12 08:35:32 -0500

    Upgrade to Rails 7.2

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,17 @@
 require_relative "boot"
 
 require "rails"
-# Jut the frameworks we're using:
+# Pick the frameworks you want:
 require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
-require "action_cable/engine"
+# require "action_cable/engine"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,7 +26,7 @@ module Purl
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
