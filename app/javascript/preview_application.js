2b17d7d37c4d22382c14b915d79e5c43a28ffd06
Justin Coyne <jcoyne@justincoyne.com>
2025-09-03 13:42:02 -0500

    Remove the ability to preview mods
    As we migrate to Cocina for metadata display, this will no longer work

--- app/javascript/preview_application.js
+++
@@ -1,3 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// This is just for preview to separate out its JS imports.
-import "preview_controllers"
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,8 +6,5 @@ pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/inde
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/preview_controllers", under: "preview_controllers"
-# Preview is a separate application to avoid loading ace-builds for every page
-pin 'preview_application'
 pin "ace-builds", to: "https://ga.jspm.io/npm:ace-builds@1.16.0/src-noconflict/ace.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
