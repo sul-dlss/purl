88c5f98c40a80153e25245044dcef0c1239c97ec
Justin Coyne <jcoyne@justincoyne.com>
2025-09-03 09:23:25 -0500

    Extract components from the show page
    This will make it easier to test the show page without having to do feature tests for each case. Feature tests are much slower than component tests

---
+++ app/components/show/sidebar_component.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Show
+  class SidebarComponent < ViewComponent::Base
+    def initialize(version:, purl:)
+      @version = version
+      @purl = purl
+      super()
+    end
+
+    attr_accessor :version, :purl
+
+    delegate :released_to_searchworks?, to: :purl
+
+    def metrics?
+      version.embeddable? || version.show_download_metrics? || version.doi.present?
+    end
+  end
+end
