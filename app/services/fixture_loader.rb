0ef572bcbe77083018751ca71d1341540cfc0e2f
Justin Coyne <jcoyne@justincoyne.com>
2025-09-05 14:39:14 -0500

    Rename document_cache to stacks
    document_cache is a relic of the /purl filesystem. We no longer read from /purl, so we can remove that directory

--- app/services/fixture_loader.rb
+++ app/services/fixture_loader.rb
@@ -4,8 +4,8 @@ class FixtureLoader
   def self.load(druid, machine: 'purl-fetcher-prod')
     # Load fixture data for the given druid
     tree = Dor::Util.create_pair_tree(druid)
-    FileUtils.rm_f("spec/fixtures/document_cache/#{tree}")
-    dest = "spec/fixtures/document_cache/#{tree}/#{druid}"
+    FileUtils.rm_f("spec/fixtures/stacks/#{tree}")
+    dest = "spec/fixtures/stacks/#{tree}/#{druid}"
     src = "/stacks/#{tree}/#{druid}/versions"
 
     FileUtils.mkdir_p(dest)
