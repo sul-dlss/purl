cd43a7ad3303f8c46450694b1f950d56d6549328
Nick Budak <budak@stanford.edu>
2025-09-18 11:44:22 -0700

    Separate the related resource from its parent component

--- app/components/bibliographic_component.rb
+++ app/components/bibliographic_component.rb
@@ -9,16 +9,12 @@ class BibliographicComponent < ViewComponent::Base
   attr_reader :version
 
   delegate :cocina_display, :mods, to: :version
-  delegate :general_note_display_data, :identifier_display_data, :access_display_data, to: :cocina_display
+  delegate :general_note_display_data, :identifier_display_data, :access_display_data, :related_resource_display_data, to: :cocina_display
 
   def render?
     general_note_display_data.present? ||
-      related_fields.present? ||
+      related_resource_display_data.present? ||
       identifier_display_data.present? ||
       access_display_data.present?
-  end
-
-  def related_fields
-    cocina_display.related_resource_display_data
   end
 end
