54de22e8fee472df842cbc281ef2ea7675e345e5
Justin Coyne <jcoyne@justincoyne.com>
2025-09-23 13:04:32 -0500

    Render RelatedResourcesFieldComponent as a collection

--- app/components/show/description/related_resources_field_component.rb
+++ app/components/show/description/related_resources_field_component.rb
@@ -4,19 +4,32 @@ module Show
   module Description
     # A component for rendering resources related to the current object.
     class RelatedResourcesFieldComponent < ViewComponent::Base
-      def initialize(values:)
-        @values = values
+      # @param [CocinaDisplay::DisplayData] related_resources_field
+      def initialize(related_resources_field:)
+        @related_resources_field = related_resources_field
         super()
       end
 
-      attr_accessor :values
+      attr_accessor :related_resources_field
+
+      delegate :label, to: :related_resources_field
 
       def render?
-        values.present?
+        related_resources_field.present?
+      end
+
+      # @return [Array<CocinaDisplay::RelatedResource>]
+      def related_resources
+        related_resources_field.objects
+      end
+
+      def child_components
+        related_resources.map { child_component it }
       end
 
       # If the related resource has a URL, render it as a labelled link.
       # Otherwise, render it using the nested presentation (e.g. Parker citations).
+      # @param [CocinaDisplay::RelatedResource] related_resource
       def child_component(related_resource)
         if related_resource.url?
           LabelledLinkComponent.new(url: related_resource.url, link_text: related_resource.to_s)
