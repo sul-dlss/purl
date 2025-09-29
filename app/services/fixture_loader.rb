1965c359dd9707e263993cd4855dab4328ec0b61
dnoneill <noneill@stanford.edu>
2025-09-29 16:24:44 -0400

    stop trying to delete files that do not exist

--- app/services/fixture_loader.rb
+++ app/services/fixture_loader.rb
@@ -10,7 +10,5 @@ class FixtureLoader
 
     FileUtils.mkdir_p(dest)
     `scp -r lyberadmin@#{machine}:#{src} #{dest}`
-    FileUtils.rm "#{dest}/versions/cocina.json"
-    FileUtils.rm "#{dest}/versions/public.xml"
   end
 end
