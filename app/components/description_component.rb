86e9155d032c266ef639d9c8b5bb89efd91d4368
Nick Budak <budak@stanford.edu>
2026-02-27 11:13:49 -0800

    Refactor to use new publication_display_data helper
    And update to cocina_display v1.10

--- app/components/description_component.rb
+++ app/components/description_component.rb
@@ -9,8 +9,6 @@ class DescriptionComponent < ViewComponent::Base
   attr_reader :version
 
   delegate :cocina_display, to: :version
-  delegate :form_display_data, :form_note_display_data, :language_display_data, :map_display_data, :event_note_display_data, :event_date_display_data,
-           to: :cocina_display
 
   def label_id
     'section-description'
@@ -23,28 +21,19 @@ class DescriptionComponent < ViewComponent::Base
   def field_map
     @field_map ||= [
       [title_display_data, COMMA],
-      [form_display_data, SEMICOLON],
-      [form_note_display_data, COMMA],
-      [publication_places, nil],
-      [publisher, COMMA],
-      [event_date_display_data, SEMICOLON],
-      [event_note_display_data, COMMA],
-      [language_display_data, SEMICOLON],
-      [map_display_data, COMMA]
+      [cocina_display.form_display_data, SEMICOLON],
+      [cocina_display.form_note_display_data, COMMA],
+      [cocina_display.publication_display_data, COMMA],
+      [cocina_display.event_date_display_data, SEMICOLON],
+      [cocina_display.event_note_display_data, COMMA],
+      [cocina_display.language_display_data, SEMICOLON],
+      [cocina_display.map_display_data, COMMA]
     ].select { |field, _| field.present? }
   end
 
   # All the titles, except the main title
   def title_display_data
     cocina_display.title_display_data.reject { it.label == 'Title' }
-  end
-
-  def publication_places
-    CocinaDisplay::DisplayData.from_strings(cocina_display.publication_places, label: 'Place').presence
-  end
-
-  def publisher
-    CocinaDisplay::DisplayData.from_strings(cocina_display.publisher_names, label: 'Publisher').presence
   end
 
   def render?
