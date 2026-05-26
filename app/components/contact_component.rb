c4adf2aa334995355e58e05156c14d09757174f0
Nick Budak <budak@stanford.edu>
2026-05-26 12:37:59 -0700

    Correctly render multiple contact emails
    This ports the same logic/template used in Searchworks, which
    is correct. Tested on yr306cd5969.

--- app/components/contact_component.rb
+++ app/components/contact_component.rb
@@ -9,9 +9,8 @@ class ContactComponent < ViewComponent::Base
   attr_reader :version
 
   delegate :cocina_display, to: :version
-  delegate :contact_email_display_data, to: :cocina_display
 
   def render?
-    contact_email_display_data.present?
+    cocina_display&.contact_email_display_data.present?
   end
 end
